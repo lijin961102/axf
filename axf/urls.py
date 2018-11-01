f86e51678cb8dd8c20e14d9c84d07e8f5f5d9fae
LIjin <980200177@qq.com>
2018-11-01 15:28:24 +0800

    AXF项目基本功能完成（轮播图、导航、每日必购）

--- axf/urls.py
+++ axf/urls.py
@@ -4,10 +4,8 @@ from axf import views
 app_name = 'axf'
 
 urlpatterns = [
-    url(r'^$',views.index,name='index'),
-    url(r'^base/$',views.base,name='base'),
-    url(r'^home/$',views.home,name='home'),
-    url(r'^market/$',views.market,name='market'),
-    url(r'^cart/$',views.cart,name='cart'),
-    url(r'^mine/$',views.mine,name='mine'),
+    url(r'^$',views.home,name='home'), # 首页
+    url(r'^market/$',views.market,name='market'), # 闪购超市
+    url(r'^cart/$',views.cart,name='cart'), # 购物车
+    url(r'^mine/$',views.mine,name='mine'), # 我的
 ]
--- axf/views.py
+++ axf/views.py
@@ -1,27 +1,41 @@
 from django.shortcuts import render
 
 # Create your views here.
+from axf.models import Wheel, Nav, Mustbuy
+
+
 def base(request):
     return render(request,'base/base.html')
 
-def index(request):
-    return None
-
-
+# 首页
 def home(request):
+    # 轮播图数据
+    wheels = Wheel.objects.all()
 
-    return render(request,'home/home.html')
+    # 导航数据
+    navs = Nav.objects.all()
+
+    # 每日必购
+    mustbuys = Mustbuy.objects.all()
+
+    data  ={
+        'wheels':wheels,
+        'navs':navs,
+        'mustbuys':mustbuys,
+    }
+
+    return render(request,'home/home.html',context=data)
 
 
 def market(request):
-    return None
+    return render(request,'market/market.html')
 
 
 def cart(request):
-    return None
+    return render(request,'cart/cart.html')
 
 
 def mine(request):
-    return None
+    return render(request,'mine/mine.html')
 
 
